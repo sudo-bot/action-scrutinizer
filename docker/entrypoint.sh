187a98c6b08b270eebe8ed0b9a390483ed0f52a1
William Desportes <williamdes@wdes.fr>
2024-07-05 10:46:34 +0200

    Improve docker labels and move safe git dir to entrypoint

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -1,13 +1,16 @@
 #!/bin/sh -l
+
 ##
 # @license http://unlicense.org/UNLICENSE The UNLICENSE
 # @author William Desportes <williamdes@wdes.fr>
 ##
 
-set -e
+set -eu
 
 CLI_ARGS="$1"
 
 echo "::debug CLI_ARGS: ${CLI_ARGS}"
+
+git config --global --add safe.directory $PWD
 
 ocular code-coverage:upload ${CLI_ARGS}
